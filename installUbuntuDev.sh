a078f97449e9cc8974237cd3a5c59afffdcdeafa
Rafael Garcia SantAnna <rgarciasantanna@gmail.com>
2024-03-23 12:38:20 -0300

    Adicionando novas funcionalidades

--- installUbuntuDev.sh
+++ installUbuntuDev.sh
@@ -40,6 +40,15 @@ sudo apt install snapd -y;
     #Postman#
     sudo snap install postman;
 
+    #standard-notes#
+    sudo snap install standard-notes;
+
+    #docker#
+    sudo snap install docker;
+
+    #pycharm#
+    sudo snap install pycharm-community --classic;
+
 ##Download Apps##
     ##create and access path to dowload files##
     mkdir ~/Download/apps
@@ -50,6 +59,32 @@ sudo apt install snapd -y;
 
     ##Install All 
     sudo dpkg -i *.deb;
+
+    ##Java 8
+    wget -c https://download.oracle.com/otn/java/jdk/8u202-b08/1961070e4c9b4e26a04e7f5a083f551e/jdk-8u202-linux-x64.tar.gz;
+
+    ##Java 11
+    wget -c https://www.oracle.com/webapps/redirect/signon?nexturl=https://download.oracle.com/otn/java/jdk/11.0.21%2B9/8819d0447e4d41b3bd1d9e1007728d17/jdk-11.0.21_linux-x64_bin.tar.gz;
+
+    ##Java 17
+    wget -c https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.tar.gz;
+
+    ## Maven 
+    wget -c https://dlcdn.apache.org/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.tar.gz;
+
+
+    sudo chmod -R 775 jdk-8u202-linux-x64.tar.gz jdk-11.0.21_linux-x64_bin.tar.gz jdk-17_linux-x64_bin.tar.gz apache-maven-3.9.6-bin.tar.gz
+
+    sudo mv jdk-8u202-linux-x64.tar.gz jdk-11.0.21_linux-x64_bin.tar.gz jdk-17_linux-x64_bin.tar.gz apache-maven-3.9.6-bin.tar.gz /opt
+
+    cd /opt
+
+    tar -xvf jdk-8u202-linux-x64.tar.gz
+    tar -xvf jdk-11.0.21_linux-x64_bin.tar.gz
+    tar -xvf jdk-17_linux-x64_bin.tar.gz
+    tar -xvf apache-maven-3.9.6-bin.tar.gz
+
+    sudo chmod -R 775 /opt
 
 
 ##Update##
